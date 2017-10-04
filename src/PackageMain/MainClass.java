7ec95090d50314742803caf67652b3c6f3ac61e4
IUser <sashayenza1996@gmail.com>
2017-10-04 15:30:27 +0300

    Initial commit

---
+++ src/PackageMain/MainClass.java
@@ -0,0 +1,8 @@
+public class MainClass
+{
+    public static void main (String[] args)
+    {
+        PrintString printString = new PrintString();
+        printString.print("Hello World");
+    }
+}
---
+++ src/PackagePrint/PrintString.java
@@ -0,0 +1,7 @@
+public class PrintString
+{
+    public void print(String str)
+    {
+        System.out.println("Your string is: "+ str+ "\n");
+    }
+}
