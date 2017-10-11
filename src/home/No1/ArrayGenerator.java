96ff2b8b0dc7e967d5be129cc73c9067ec3a7c2a
IUser <sashayenza1996@gmail.com>
2017-10-11 22:09:47 +0300

    + уникальные значения + операции + поиск + печать + выход + меню + заменил генератор чисел
    - нет ввода чисел вручную
    - отсутствует выбора размера массива
    - много статика
    - не сделана оптимизация
    - говнокод в лоб
    - нет инкапсуляции
    - нет проверки на вводимые значения (я же бывший тестировщик)
    
    ? в условии сказано :
    "Вести до 10 уникальных целых цифр, для хранения использовать массив "
    Цифр- их всего 10, но судя по чату я понял (возможно ошибочно), что имелось ввиду чисел.
    
    ! очень надеюсь на обратку, чтобы подробно узнать свои ошибки и как правильно писать.

--- src/home/No1/ArrayGenerator.java
+++ src/home/No1/ArrayGenerator.java
@@ -1,10 +1,25 @@
 package No1;
 
+import java.util.Random;
+
 public class ArrayGenerator {
     public static int [] arrayGenerator(){
-        int [] array = new int[(int) (Math.random()*9)];
+        Random generator = new Random();
+        int [] array = new int[(int) generator.nextInt(10)];
+        boolean flag;
+        int number;
         for (int i=0; i<array.length; i++){
-            array[i] = (int) (Math.random()*9);
+            number = generator.nextInt( 100 );
+            flag=true;
+            for(int j=0; j<=i; j++) {
+                if(number==array[j])
+                    flag=false;
+            }
+            if(flag){
+                array[i]=number;
+            }else{
+                i--;
+            }
         }
         return array;
     }
