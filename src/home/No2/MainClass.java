6c6c1f737d655e115507f88c1cdc5b952ad2e93b
IUser <sashayenza1996@gmail.com>
2017-10-19 17:45:42 +0300

    завтра ссылки
    - stream
    - lambda еще практика
    - post 247015 хабр
    
    + все условия соблюдены, мб смогу выспаться хоть сегодня
    
    !компараторы вещь непривычная

--- src/home/No2/MainClass.java
+++ src/home/No2/MainClass.java
@@ -7,5 +7,9 @@ public class MainClass {
     public static void main (String[] args){
         Methods methods = new RealizationMethodsList();
         new RealizationMethodsList().run();
+
+        // совсем забыл про Map, но основная идея для полной реализации и list, и map- RealizationMethod можно сделать не только для list, а в качетве параметра принимать саму Collection
+        //хотел так сделать, но не хватило времени и , особенно, знаний итераторов. тогда можно было бы не переписывать один и тот же код для list и map отдельно.
+        //P.S. слак недоступен. не знаю причину.
     }
 }
